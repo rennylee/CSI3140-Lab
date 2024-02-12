f5d7f1b0059d747f7de41b31b1bfb97f90003848
Eeshaan Patel <epate039@uottawa.ca>
2024-02-11 23:20:37 -0500

    Stuff

--- Assignment3/e6.18.js
+++
@@ -1,9 +0,0 @@
-let num1 = Number(prompt("First number:"));
-let num2 = Number(prompt("Second number:"));
-let num3 = Number(prompt("Third number:"));
-alert(
-    "Sum:" + (num1 + num2 + num3) + "\n" +
-    "Average:" + ((num1 + num2 + num3)/3) + "\n" +
-    "Smallest:" + Math.min(num1,num2,num3) + "\n" +
-    "Largest:" + Math.max(num1,num2,num3) + "\n"
- )
